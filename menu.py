42baa9a7d4beaadce1542a65d8fdd35e7f2e0cb0
Nikolay <chort.krsk@rambler.ru>
2023-01-11 22:03:54 +0700

    looks like work.part2

--- menu.py
+++ menu.py
@@ -1,11 +1,21 @@
 from read import read_cont as rc
 from write import style1 as s1
 from write import style2 as s2
+from read_book import read1 as r1
+from read_book import read1 as r2
 
 def menu():
-    adr = rc()
-    a = int(input('Enter method of write? (1-line by line, 2-in one term) '))
-    if a == 1:
-        s1(adr)
+    b = int(input('Wonna write or read? (1-Write, 2-Read) '))
+    if b == 1:
+        adr = rc()
+        a = int(input('Enter method of write? (1-line by line, 2-in one term) '))
+        if a == 1:
+            s1(adr)
+        else:
+            s2(adr)
     else:
-        s2(adr)
+        c = int(input('Enter file want to read? (1-Book1, 2-Book2) '))
+        if c == 1:
+            r1()
+        else:
+            r2()
